3f646bf6765a7869ec8e13273084f1e887915d72
root <runmass@gmail.com>
2023-12-12 14:32:14 -0500

    Adding Prometheus and Node Exporter roles, various other fixes

--- roles/grafana/files/etc/firewall.d/ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="192.168.8.0/24" port port="22" protocol="tcp" accept'
-
--- roles/grafana/files/etc/firewall.sh
+++ roles/grafana/files/etc/firewall.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-firewall-cmd --complete-reload
-
+# First the default zone is being set to drop, so that only the specified connections will be allowed
 firewall-cmd --set-default-zone=drop
 
+# This varialbe allows only newlines to be recognized as boundaries, without it existing rich rules wouldn't be removed properly due to spaces in between
+IFS=$'\n'
+
+# Here we simply loop over the existing rich rules and remove each one individually
+existing_rules=$(firewall-cmd --list-rich-rules)
+for rule in $existing_rules; do
+    firewall-cmd --permanent --remove-rich-rule="$rule"
+done
+
+# Now we apply all rich rules from /etc/firewall.d
 for script in /etc/firewall.d/*
 do
   if [[ -x "$script" ]]; then
@@ -11,5 +20,5 @@ do
   fi
 done
 
+# And reload the firewall
 firewall-cmd --reload
-
